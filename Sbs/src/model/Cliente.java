70f2bc8456b39b07f331521b190145edf0be5078
viniciusalopes <suporte@viniciusalopes.com.br>
2019-12-06 15:43:38 -0300

    Model

---
+++ Sbs/src/model/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author vovostudio
+ */
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private String email;
+
+    public Cliente(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return nome + " [" + email + "]";
+    }
+}
